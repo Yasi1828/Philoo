110aa1e80b5b5b1f05312ec2873fe6adb01c7e2a
jasmine <yasigh387@gmail.com>
2025-02-15 14:24:07 +0330

    added models and created migrations

--- PhiloApp/models.py
+++ PhiloApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Philosophers(models.Model):
+    FullName = models.CharField(max_length = 100)
+    BirthDate = models.TextField()
+    Bio = models.TextField()
+    KeyIdeas = models.TextField()
+class Schools(models.Model):
+    Name = models.CharField(max_length = 100)
+    Concept = models.TextField()
+
+    
