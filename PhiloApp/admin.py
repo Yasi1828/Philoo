16426101001a23f5bf8acb307cb515d90078a286
jasmine <yasigh387@gmail.com>
2025-02-16 15:11:59 +0330

    small changes for image in html and admin.py

--- PhiloApp/admin.py
+++ PhiloApp/admin.py
@@ -5,4 +5,4 @@ class PhilosopherAdmin(admin.ModelAdmin):
     list_display = ('FullName', 'School', 'picture')  
     
 admin.site.register(Schools)
-admin.site.register(Philosophers,PhilosopherAdmin)
+admin.site.register(Philosophers)
