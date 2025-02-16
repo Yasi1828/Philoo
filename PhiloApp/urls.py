b823790667ca0ecccb8face090aa339b89b8b079
jasmine <yasigh387@gmail.com>
2025-02-16 10:55:06 +0330

    added new htmls

--- PhiloApp/urls.py
+++ PhiloApp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # Example view
-]
 
+    # path('philosopher/<int:philosopher_id>/', views.philosopher_detail, name='philosopher_detail'),
+    # path('school/<int:school_id>/', views.school_detail, name='school_detail'),
+]
