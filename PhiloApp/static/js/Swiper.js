ccc316bbeb2c244617abbda4d6cb6069d4308201
jasmine <yasigh387@gmail.com>
2025-02-25 15:32:28 +0330

    fixed the html loop and slider and the cards sizes

--- PhiloApp/static/js/Swiper.js
+++ PhiloApp/static/js/Swiper.js
@@ -1,13 +1,12 @@
 const swiper = new Swiper(".swiper-container", {
-  slidesPerView: "auto", // This will make the slides adjust based on their content
-  spaceBetween: 20, // Adjust the spacing between slides
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+  slidesPerView: 1,
+  spaceBetween: 10,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
-  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
