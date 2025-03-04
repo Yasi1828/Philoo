c462eb6016afd66f215081868831c5d533943299
jasmine <yasigh387@gmail.com>
2025-03-04 15:08:19 +0330

    updated app.views and app.urls

--- PhiloApp/urls.py
+++ PhiloApp/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
     path('', views.index, name='index'),  # Example view
 
     path('philosopher/<int:philosopher_id>/', views.philosopher_detail, name='philosopher_detail'),
+    
     path('school/<int:school_id>/', views.school_detail, name='school_detail'),
+    
+    path('school/', views.school_list, name='school_list'),
+    
+    path('philosopher/', views.philosopher_list, name='philosopher_list'),
 ]
--- PhiloApp/views.py
+++ PhiloApp/views.py
@@ -19,5 +19,15 @@ def school_detail(request, school_id):
     school = get_object_or_404(Schools, id=school_id)
     return render(request, 'school_detail.html', {'school': school})
 
+def school_list(request, schoollist_id):
+    schools = Schools.objects.all()
+    context = {'Schools' : schools}
+    return render(request, 'school_list.html', context=context)
+
+def philosopher_list(request, philosopherlist_id):
+    philosophers = Philosophers.objects.all()
+    context = {'Philosophers' : philosophers}
+    return render(request, 'philosopher_list.html', context=context)
+
 
 
