7115d043c6864f8e52efc447131c64a2f5cd7069
jasmine <yasigh387@gmail.com>
2025-03-04 15:18:40 +0330

    removed id as the argument of views

--- PhiloApp/views.py
+++ PhiloApp/views.py
@@ -19,12 +19,12 @@ def school_detail(request, school_id):
     school = get_object_or_404(Schools, id=school_id)
     return render(request, 'school_detail.html', {'school': school})
 
-def school_list(request, schoollist_id):
+def school_list(request):
     schools = Schools.objects.all()
     context = {'Schools' : schools}
     return render(request, 'school_list.html', context=context)
 
-def philosopher_list(request, philosopherlist_id):
+def philosopher_list(request):
     philosophers = Philosophers.objects.all()
     context = {'Philosophers' : philosophers}
     return render(request, 'philosopher_list.html', context=context)
