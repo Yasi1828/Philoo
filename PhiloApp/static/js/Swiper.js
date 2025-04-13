58a3996b22afb0680dd2629c8bf0f22a78af3049
jasmine <yasigh387@gmail.com>
2025-04-13 12:21:52 +0330

    made some minor changes and deleted redundant codes

--- PhiloApp/static/js/Swiper.js
+++ PhiloApp/static/js/Swiper.js
@@ -1,20 +1,22 @@
 const swiper = new Swiper(".swiper-container", {
-  loop: true,
-  slidesPerView: 3,
-  slidesPerGroup: 1,
+  loop: false,
+  slidesPerView: "auto",
   spaceBetween: 30,
   centeredSlides: true,
-  loopedSlides: 3,
-  // centeredSlides: true,
 
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-    dynamicBullets: true, // Enable dynamic bullets
+    dynamicBullets: true,
     dynamicMainBullets: 5,
   },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      spaceBetween: 20,
+    },
   },
 });
