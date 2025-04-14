38b2270125729c7c418fc09086788e7eac568b73
jasmine <yasigh387@gmail.com>
2025-04-14 10:02:12 +0330

    fixed card scaling while hovering

--- PhiloApp/static/js/Swiper.js
+++ PhiloApp/static/js/Swiper.js
@@ -3,7 +3,8 @@ const swiper = new Swiper(".swiper-container", {
   slidesPerView: "auto",
   spaceBetween: 20,
   centeredSlides: true,
-
+  noSwiping: true,
+  noSwipingClass: "philosopher-card",
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
